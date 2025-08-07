9a3bd96a1cdb37663b4d7265cb630b08874952c3
luanfv <luanfrancavieira@gmail.com>
2025-08-07 19:49:18 -0300

    refactor: path

--- backend/src/catalog/catalog.module.ts
+++ backend/src/catalog/catalog.module.ts
@@ -3,8 +3,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import OpenAI from 'openai';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StoreRepository } from './infra/repository/store.repository';
-import { Store } from '../shared/infra/database/entity/store.entity';
-import { Product } from '../shared/infra/database/entity/product.entity';
+import { Store } from '@shared/infra/database/entity/store.entity';
+import { Product } from '@shared/infra/database/entity/product.entity';
 import { CatalogRepository } from './infra/repository/catalog.repository';
 import { CatalogController } from './infra/api/catalog.controller';
 
